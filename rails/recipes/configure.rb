f1c4bbc6e00e3dbe2c1bd363c4e343a9714167e4
Andrew Porter <partydrone@me.com>
2015-04-29 23:16:54 -0600

    Create configure.rb

---
+++ rails/recipes/configure.rb
@@ -0,0 +1,30 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
+  deploy = node[:deploy][application]
+
+  template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+    source "secrets.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(
+      :environment => deploy[:rails_env]
+    )
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      deploy[:secrets][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+end
